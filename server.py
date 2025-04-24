eca853dca8cc288a4fd369cfe52bdfb8d11c7adc
hamedops <ahmadinia.hamed@gmail.com>
2025-04-25 00:34:50 +0330

    roll back

--- server.py
+++ server.py
@@ -7,5 +7,5 @@ def home():
     return "Hello, World! Welcome to my simple web server."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
