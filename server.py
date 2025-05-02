5ec14ebad9c68bed29a31cc508a8052588141a37
hamedops <ahmadinia.hamed@gmail.com>
2025-05-02 16:46:24 +0330

    edit server file

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! Welcome to my simple web server."
+    return "Hello, World! Welcome to HamedOps Channal."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
